352807b09f4922a416e5b2b192eaa74a29dd7e6f
supa <105751066+wednesdaytruth@users.noreply.github.com>
2023-05-03 15:39:44 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6208,7 +6208,6 @@ function library:createwatermark(info)
     watermark.objects.outline2 = library:outline(watermark.objects.outline1, Color3.fromRGB(0,0,0), 10, true)
 
     watermark.objects.text2 = library:create("Text", { Parent = watermark.objects.background, Visible = true, Transparency = 1, Theme = "Accent", Size = 13, Text = title, Center = false, Outline = false, Font = Drawing.Fonts.Plex, Position = UDim2.new(0, 3,0,2), ZIndex = 11})
-    watermark.objects.text3 = library:create("Text", { Text = title, Parent = watermark.objects.background, Visible = true, Transparency = 1, Theme = "Text", Size = 13, Center = false, Outline = false, Font = Drawing.Fonts.Plex, Position = UDim2.new(0, 1 + utility.textlength(watermark.objects.text2.Text, 2, 13).X,0,2), ZIndex = 11})
     --
     function watermark.setstate(bool)
         for i, v in next, watermark.objects do
